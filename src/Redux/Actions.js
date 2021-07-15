172e7b2d164bdad0f84c9493859456699b6f7242
AlirezaGhadyani <alirezaghadyani16@gmail.com>
2021-07-15 22:45:33 +0430

    set data in local storeage and user status

--- src/Redux/Actions.js
+++ src/Redux/Actions.js
@@ -1,6 +1,20 @@
+export const setUserStatus = ( status ) => {
+    return {
+        type: "SET_USER_STATUS",
+        payload: status
+    };
+}
+
 export const setModalStatus = ( modalStatus ) => {
     return {
         type: "SET_MODAL_STATUS",
         payload: modalStatus
+    };
+}
+
+export const setUserData = ( userData ) => {
+    return {
+        type: "SET_USER_DATA",
+        payload: userData
     };
 }
--- src/Redux/Reducer.js
+++ src/Redux/Reducer.js
@@ -1,17 +1,27 @@
 const initialState = {
+    userStatus: '',
     modalStatus: [{
         showModal: false,
         type: '',
         status: '',
         message: ``,
         btnLabel: '',
-    }]
+    }],
+    userData: []
 };
+
+// const saveUserDataToLocalStorage = ( state, payload ) => {
+//     {}
+// }
 
 const AppReducer = ( state = initialState, { type, payload } ) => {
     switch ( type ) {
+        case "SET_USER_STATUS":
+            return { ...state, userStatus: payload };
         case "SET_MODAL_STATUS":
             return { ...state, modalStatus: payload };
+        case "SET_USER_DATA":
+            return { ...state, userData: payload };
         default:
             return state
     }
